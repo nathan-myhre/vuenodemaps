852c2b346b288727ca6b461290ce2b8488b4fbdd
Nathan <nmyhre@revenuewire.com>
2019-02-07 21:39:16 -0800

    Add Home component and render Mapbox GL map

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Home msg="Checkout my favorite lunch spot!" api-key="pk.eyJ1IjoiZGFub2hhbW1lciIsImEiOiJjaW5jMWYza3owbzAwdTNsd29vNnozZmtsIn0.L_pKW48Ot590vIBgFemTTQ"/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div id="map"></div>
+
+  </div>
+</template>
+
+<script>
+  import mapboxgl from 'mapbox-gl'
+
+  export default {
+  name: 'Home',
+  props: {
+    msg: String,
+    apiKey: String
+  },
+  mounted() {
+      this.createMap()
+  },
+  methods: {
+      //Draw map on screen
+      createMap() {
+          mapboxgl.accessToken = this.apiKey
+          this.map = new mapboxgl.Map({
+              container: 'map',
+              minzoom: 1.3,
+              style: 'mapbox://styles/mapbox/streets-v9',
+              center: [-123.329773, 48.4284],
+              zoom: 11,
+          })
+
+      },
+  }
+}
+</script>
+
+<style scoped>
+  #map {
+    position:relative;
+    top: 0;
+    bottom: 0;
+    width: 90%;
+    height: 100vh;
+    margin:0;
+  }
+</style>
